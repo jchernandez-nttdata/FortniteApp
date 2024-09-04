ba4ef844fc77f588be3d0992fa138e9a2546d9bb
Juan Carlos Hernandez Castillo <jherncas@emeal.nttdata.com>
2024-09-04 10:25:43 -0500

    Add http method enum

---
+++ FortniteApp/Utils/Enums/HttpMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HttpMethod.swift
+//  FortniteApp
+//
+//  Created by Juan Carlos Hernandez Castillo on 3/09/24.
+//
+
+import Foundation
+
+/// Represents HTTP methods used in network requests
+enum HttpMethod: String {
+    case GET, POST, DELETE, PUT
+}
