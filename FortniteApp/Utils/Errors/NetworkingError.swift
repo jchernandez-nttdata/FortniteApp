c69f82096a0be5ab398d198516732dd0dfed8c27
Juan Carlos Hernandez Castillo <jherncas@emeal.nttdata.com>
2024-09-04 10:25:55 -0500

    Add networking errors

---
+++ FortniteApp/Utils/Errors/NetworkingError.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkingError.swift
+//  FortniteApp
+//
+//  Created by Juan Carlos Hernandez Castillo on 3/09/24.
+//
+
+import Foundation
+
+/// An enumeration representing possible errors that can occur during network operations.
+enum NetworkingError: Error {
+    case invalidURL
+    case requestFailed(statusCode: Int)
+    case decodeFailure
+    case invalidResponse
+}
