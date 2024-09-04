eb87e62cccd07b430af397c7698123d8d73b525b
Juan Carlos Hernandez Castillo <jherncas@emeal.nttdata.com>
2024-09-04 15:12:51 -0500

    Add tournaments models

---
+++ FortniteApp/Models/EventWindow.swift
@@ -0,0 +1,14 @@
+//
+//  EventWindow.swift
+//  FortniteApp
+//
+//  Created by Juan Carlos Hernandez Castillo on 3/09/24.
+//
+
+import Foundation
+
+struct EventWindow: Decodable {
+    let windowId: String
+    let beginTime: Date
+    let endTime: Date
+}
---
+++ FortniteApp/Models/EventsResponse.swift
@@ -0,0 +1,53 @@
+//
+//  EventsResponse.swift
+//  FortniteApp
+//
+//  Created by Juan Carlos Hernandez Castillo on 3/09/24.
+//
+
+import Foundation
+
+struct EventsResponse: Decodable {
+    let events: [Event]
+}
+
+struct Event: Decodable {
+    let id: String
+    let beginTime: Date
+    let endTime: Date
+    let poster: String?
+    let shortDescription: String
+    let nameLine1: String
+    let nameLine2: String
+    let windows: [EventWindow]
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, beginTime, endTime, poster, windows
+        case nameLine1 = "name_line1"
+        case nameLine2 = "name_line2"
+        case shortDescription = "short_description"
+    }
+}
+
+extension Event {
+    var title: String {
+        return nameLine1 + nameLine2
+    }
+    
+    var sortedWindows: [EventWindow] {
+        return windows.sorted { $0.beginTime < $1.beginTime }
+    }
+    
+    var currentOrNextWindowEvent: EventWindow? {
+        let currentDate = Date()
+        
+        // validates if the event is already finish
+        if currentDate > endTime { return nil }
+        
+        return sortedWindows.first { window in
+            (window.beginTime <= currentDate && window.endTime > currentDate) ||
+            window.beginTime > currentDate
+        }
+        
+    }
+}
