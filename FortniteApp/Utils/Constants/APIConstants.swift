3dda23b0813090e81224cffff0eb4096280a40b3
Juan Carlos Hernandez Castillo <jherncas@emeal.nttdata.com>
2024-09-04 15:10:47 -0500

    Add API constants file

---
+++ FortniteApp/Utils/Constants/APIConstants.swift
@@ -0,0 +1,17 @@
+//
+//  APIConstants.swift
+//  FortniteApp
+//
+//  Created by Juan Carlos Hernandez Castillo on 3/09/24.
+//
+
+import Foundation
+
+/// Static API constants variables
+struct APIConstants {
+    /// Base URL for API requests
+    static let baseURL = "https://fortniteapi.io/v1/"
+    
+    //TODO: check another option to save apikey
+    static let apiKey = "api-key"
+}
