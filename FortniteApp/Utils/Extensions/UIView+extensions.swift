ae1a1233fb1b08e930ea4101f1392398208cd314
Juan Carlos Hernandez Castillo <jherncas@emeal.nttdata.com>
2024-09-09 11:11:57 -0500

    Fix pin method on UIView extension

--- FortniteApp/Utils/Extensions/UIView+extensions.swift
+++ FortniteApp/Utils/Extensions/UIView+extensions.swift
@@ -9,16 +9,16 @@ import UIKit
 
 extension UIView {
     /// Pins this view to it's superview.
-    func pinToSuperview() {
+    func pinToSuperview(constant: CGFloat = 0) {
         guard let superview = superview else { fatalError("UIView+pinToSuperview: \(description) has no superview.") }
-        pin(to: superview)
+        pin(to: superview, constant: constant)
     }
     
     /// Pins this view to another view.
-    func pin(to view: UIView) {
-        leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    func pin(to view: UIView, constant: CGFloat = 0) {
+        leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: constant).isActive = true
+        trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -constant).isActive = true
+        topAnchor.constraint(equalTo: view.topAnchor, constant: constant).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -constant).isActive = true
     }
 }
